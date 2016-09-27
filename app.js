987c46e266bcfe264ed523aeef703e4c5df99888
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-09-27 19:01:08 -0400

    cancel button works

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 angular.module('myApp', [])
 	.controller('myCtrl', function(){
 		var main = this;
-		main.baseMeal = "";
-		main.taxRate = "";
-		main.tipPercent = "";
+
+		main.submitAmounts = function(){
+			/*there needs to be an if/else statement here to reflect
+			that if the inputs are valid, then amounts in custCharges
+			and earnInfo change. Else, there is an alert to remind
+			to enter positive numbers*/
+		};
+
+		main.cancelTransaction = function(){
+			main.baseMeal = "";
+			main.taxRate = "";
+			main.tipPercent = "";
+		};
+
 	});
