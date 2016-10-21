538b0b2354b1f54873e4f7aa597ad651dc05b156
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-10-20 23:34:51 -0400

    working with

--- app.js
+++ app.js
@@ -1,48 +1,67 @@
 angular.module('myApp', ['ngRoute'])
-    .controller('newMeal', function() {
-        var main = this;
-        main.submitted = false;
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'info.html'
+        }).when('/info', {
+            templateUrl: 'info.html'
+        }).when('/newmeal', {
+            templateUrl: 'newmeal.html',
+            controller: 'newMeal',
+            controllerAs: 'main'
+        }).when('/earnings', {
+            templateUrl: 'earnings.html',
+            controller: 'myEarnings',
+            controllerAs: 'main'
+        }).when('/error', {
+        	template: '<p>Page does not exist</p>'
+        }).otherwise('/error')
+    }])
+
+.controller('newMeal', function($rootScope) {
+    var main = this;
+    main.submitted = false;
+    main.tip = 0;
+    main.tax = 0;
+    main.total = 0;
+    $rootScope.tipTotal = 0;
+    $rootScope.mealCount = 0;
+    $rootScope.average = 0;
+
+    main.submitAmounts = function(valid) {
+        if (valid) {
+            main.submitted = true;
+            var tipPercent = main.tipPercent;
+            var baseMeal = main.baseMeal;
+            var taxRate = main.taxRate;
+            main.tip = (tipPercent / 100) * baseMeal;
+            main.tax = (taxRate / 100) * baseMeal;
+            main.total = main.tip + main.tax + baseMeal;
+            $rootScope.mealCount++;
+            $rootScope.tipTotal += main.tip;
+            $rootScope.average = main.tipTotal / main.mealCount;
+        } else {
+            alert("You need to enter a positive number in all fields");
+        }
+    };
+
+    main.cancelTransaction = function() {
+        main.baseMeal = 0;
+        main.taxRate = 0;
+        main.tipPercent = 0;
         main.tip = 0;
         main.tax = 0;
         main.total = 0;
-        main.tipTotal = 0;
-        main.mealCount = 0;
-        main.average = 0;
+    };
+})
 
-        main.submitAmounts = function(valid) {
-            if (valid) {
-                main.submitted = true;
-                var tipPercent = main.tipPercent;
-                var baseMeal = main.baseMeal;
-                var taxRate = main.taxRate;
-                main.tip = (tipPercent / 100) * baseMeal;
-                main.tax = (taxRate / 100) * baseMeal;
-                main.total = main.tip + main.tax + baseMeal;
-                main.mealCount++;
-                main.tipTotal += main.tip;
-                main.average = main.tipTotal / main.mealCount;
-            } else {
-                alert("You need to enter a positive number in all fields");
-            }
-        };
-
-        main.cancelTransaction = function() {
-            main.baseMeal = 0;
-            main.taxRate = 0;
-            main.tipPercent = 0;
-            main.tip = 0;
-            main.tax = 0;
-            main.total = 0;
-        };
-    });
-
-.controller('myEarnings', function() {
+.controller('myEarnings', function($rootScope) {
     var main = this;
-
+$rootScope.attempt = "something";
     main.resetForm = function() {
         main.cancelTransaction();
-        main.tipTotal = 0;
-        main.mealCount = 0;
-        main.average = 0;
+        $rootScope.tipTotal = 0;
+        $rootScope.mealCount = 0;
+        $rootScope.average = 0;
+        
     };
 });
