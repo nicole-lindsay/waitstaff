83116f83b393c991595ecc3485f11bf1552c8aac
nicole-lindsay <nicoleyasdnil@gmail.com>
2016-10-25 19:49:52 -0400

    ashamed at how long this took me

--- app.js
+++ app.js
@@ -19,6 +19,20 @@ angular.module('myApp', ['ngRoute', 'ngAnimate'])
         }).otherwise('/error')
     }])
 
+.run(function($rootScope, $location, $timeout) {
+    $rootScope.$on('$routeChangeError', function() {
+        $location.path("/error");
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+        $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+        $timeout(function() {
+            $rootScope.isLoading = false;
+        }, 1000);
+    });
+})
+
 .controller('newMeal', function($rootScope, $window) {
     var main = this;
     main.submitted = false;
